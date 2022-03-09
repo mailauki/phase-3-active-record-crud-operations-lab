924dfec37b4e284f1621bf7a9f756879a5a98d9a
Julie Evans <juliemevans17@gmail.com>
2022-03-08 19:06:02 -0600

    commit tree 5589759bfa0496772c85661397af4ea7121d9a9e

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,53 @@
 class Movie < ActiveRecord::Base
+  def self.find_all_movies_by_year(year)
+    Movie.where(release_date: year)
+  end
 
+  #Create
+  def self.create_with_title(title)
+    Movie.create(title: title)
+  end
+
+  #Read
+  def self.first_movie
+    Movie.first
+  end
+
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_count
+    Movie.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.find_movie_with_attributes(attr)
+    Movie.find_by(attr)
+  end
+
+  def self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+  end
+
+  #Update
+  def update_with_attributes(attr)
+    self.update(attr)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  #Delete
+  def self.delete_by_id(id)
+    Movie.find(id).destroy
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
 end
